dc0d46fec9ea16c18b6b5fbef25a3e2838447601
MARIANA ARAUJO BARROS <19180120@CFP127ALUNO.LOCAL>
2020-02-19 14:06:54 -0300

    utilizando pusub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import './components/series/BoxSeries'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar';
+import BoxSeries from './components/series/BoxSeries'
+import Navbar from './components/Navbar'
 
-import BoxSeries from './components/series/BoxSeries';
+class App extends Component {
 
-
-
-
-class App extends Component{
- 
-  //método que mostra o Html para o usuário
-  render(){
+  render() {
     return (
       <div>
-        <Navbar/>
-        <BoxSeries/>
-    </div>
+        <Navbar />
+        <BoxSeries />
+      </div>
     )
   }
+
 }
 
 export default App;
--- src/components/series/BoxSeries.js
+++ src/components/series/BoxSeries.js
@@ -1,79 +1,80 @@
-import React, { Component } from 'react';
-import FormularioSeries from './FormularioSeries';
-import TabelaSeries from './TabelaSeries';
+import React, { Component } from 'react'
+import FormularioSeries from './FormularioSeries'
+import TabelaSeries from './TabelaSeries'
+
 
 class BoxSeries extends Component {
-    constructor(){
-        super()
-        this.state = {
-          series: []
-         
-    
-        }
-      }
-    
-      async componentDidMount(){
-        let resposta = await fetch('http://localhost:3000/series');
-        const series = await resposta.json();
-        this.setState({series: series})
-      }
-        enviaDados = async (serie) => {
-          console.log('enviando dados..')
-        
-          console.log(serie)
-          const params = {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(serie)
-          }
-          try{
-            const retorno = await fetch('http://localhost:3000/series', params);
-            if(retorno.status === 201){
-                console.log('enviado com sucesso')
-                serie = await retorno.json()
-                this.setState({series: [...this.state.series, serie]})
-                console.log(serie)
-            }
 
-          }catch(erro){
-              console.log(erro)
-          }
-          
-      }
-      deleta = async (id) => {
-        console.log(id)
-          const seriesAtual = this.state.series
-
-    const params ={
-      method: 'DELETE',
-    }
-    const retorno = await fetch('http://localhost:3000/series/'+ id,params)
-    if(retorno.status === 204){
-     this.setState({
-         series: seriesAtual.filter((serie) =>{
-             return serie.id !== id;
-         })
-     })
+  constructor() {
+    super()
+    this.state = {
+      series: [],
     }
   }
-    
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-4">
-                        <FormularioSeries enviaDados = {this.enviaDados}/>
-                    </div>
-                    <div className="col-md-8">
-                        <TabelaSeries series={this.state.series}deleta={this.deleta}/>
-                    </div>
-                </div>
-            </div>
 
-        )
+  async componentDidMount() {
+    let resposta = await fetch('http://localhost:3000/series')
+    const series = await resposta.json()
+    this.setState({ series: series })
+  }
+
+  enviaDados = async (serie) => {
+    console.log('enviando dados....')
+    console.log(serie)
+    const params = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(serie)
     }
+    try {
+      const retorno = await fetch('http://localhost:3000/series', params)
+      if (retorno.status === 201) {
+        console.log('enviado com sucesso')
+        serie = await retorno.json()
+        this.setState({ series: [...this.state.series, serie] })
+      }
+    } catch (erro) {
+      console.log(erro)
+    }
+
+  }
+
+  deleta = async (id) => {
+    const seriesAtual = this.state.series
+		const params = {
+			method: 'DELETE',
+		}
+		const retorno = await 
+			fetch('http://localhost:3000/series/' + id,params)
+		if(retorno.status === 204){
+			this.setState({
+        series: seriesAtual.filter((serie) => {
+          return serie.id !== id
+        })
+      })
+		}
+	}
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <FormularioSeries enviaDados={this.enviaDados} />
+          </div>
+          <div className="col-md-8">
+            <TabelaSeries 
+              series={this.state.series} 
+              deleta={this.deleta}
+              />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default BoxSeries;
+export default BoxSeries
